b4040e7d8faa51915cc4b85ca95d41651582f8dc
gfiedler2026 <gfiedler@asu.edu>
2025-04-15 06:19:30 -0700

    okay we all good.
    So side note, .gitignore the whole VS thing and it just totally irradicates everything. Good to know. I had to search up fixes until i came git revert

---
+++ UserPage/UserPage/Models/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace UserPage.Models
+{
+    public class UserViewModel
+    {
+        public string Username { get; set; }
+        public string CoinDataJson { get; set; }
+
+        public string CoinName { get; set; }
+        public string Currency { get; set; }
+    }
+}
