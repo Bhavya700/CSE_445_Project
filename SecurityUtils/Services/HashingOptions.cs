f191ecaa042b32711a6d5b08cae7330b6434d149
gfiedler2026 <gfiedler@asu.edu>
2025-04-19 22:57:18 -0700

    updatedSecurityUtils

---
+++ SecurityUtils/Services/HashingOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace SecurityUtils.Services
+{
+    public static class HashingOptions
+    {
+        public static int SaltSize =>
+            int.Parse(ConfigurationManager.AppSettings["HashSaltSize"] ?? "16");
+        public static int Iterations =>
+            int.Parse(ConfigurationManager.AppSettings["HashIterations"] ?? "10000");
+        public static int KeySize =>
+            int.Parse(ConfigurationManager.AppSettings["HashKeySize"] ?? "32");
+    }
+}
